8cba2d8a333d6dfbe7fd02c196e347e6e46b52e3
Sharvi Kalaskar <sharvikalaskar268@gmail.com>
2026-09-06 20:42:23 +0530

    Add files via upload

---
+++ 3(multiple).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    int num;
+    printf("Enter the number: \n");
+    scanf("%d", &num);
+    if(num / 3 == 0)
+    printf("Number is multiple of 3 \n");
+    else
+   printf("Number is not multiple of 3 \n");
+}
